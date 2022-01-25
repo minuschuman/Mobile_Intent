c9ebf5aa9e6fe74fd82d7c11c8fb707490f07a2b
chuman <chumanashrestha@gmail.com>
2022-01-26 00:56:19 +0545

    Activity for Intent Explicit Intent

---
+++ app/src/main/java/com/example/lab2_intent/ExplicitActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab2_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExplicitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explicit);
+    }
+}
